37115b4ae31d33dc023c2deadfc29822031e9aa0
Douglas Uretsky <douglasuretsky@gmail.com>
2019-01-15 12:16:39 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,4 +16,10 @@ class ArtistsController < ApplicationController
   def edit
     @artist = Artist.find(params[:id])
   end
+  
+    def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
 end
